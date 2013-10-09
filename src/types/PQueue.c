c9c262d8f7a5ac4c2872df8809508c4dafde44e1
David Collien <david@openlearning.com>
2013-10-09 20:18:50 +1100

    move .h to includes

--- src/types/PQueue.c
+++ src/types/PQueue.c
@@ -3,31 +3,31 @@
 #include "PQueue.h"
 
 struct _pq {
-	void *elements;
-	int size;
-	int length;	
+   void *elements;
+   int size;
+   int length; 
 };
 
 PQueue new_PQueue(int maxSize) {
-	PQueue pq = malloc(sizeof(struct _pq));
-	assert(pq != NULL);
+   PQueue pq = malloc(sizeof(struct _pq));
+   assert(pq != NULL);
 
-	pq->elements = malloc(sizeof(void *) * maxSize);
-	assert(pq->elements != NULL);
+   pq->elements = malloc(sizeof(void *) * maxSize);
+   assert(pq->elements != NULL);
 
-	pq->size = maxSize;
-	pq->length = 0;
+   pq->size = maxSize;
+   pq->length = 0;
 
-	return pq;
+   return pq;
 }
 
 void delete_PQueue(PQueue pq) {
-	free(pq->elements);
-	free(pq);
+   free(pq->elements);
+   free(pq);
 }
 
 void *remove_PQueue(PQueue pq) {
-	
+   
 }
 
 void add_PQueue(PQueue pq, void *toAdd, int priority);
