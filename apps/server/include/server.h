d40da0cedac8aad8ca5570577005672a1edb4432
David Collien <david@openlearning.com>
2013-10-16 15:37:36 +1100

    make server compile

--- apps/server/include/server.h
+++ apps/server/include/server.h
@@ -1,7 +1,8 @@
 #ifndef ALC_NET_SERVER_H_
 #define ALC_NET_SERVER_H_
 
-void init_sever();
-void loop_server();
+#define SERVER_BUF_SIZE 1024
+void init_server(void);
+void loop_server(void);
 
 #endif /* ALC_NET_SERVER_H_ */
--- apps/server/src/server.c
+++ apps/server/src/server.c
@@ -2,32 +2,36 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
 static int sockfd,n;
 static struct sockaddr_in servaddr,cliaddr;
 static socklen_t len;
-static char mesg[1000];
+static char mesg[SERVER_BUF_SIZE];
+
+#define PORT 32000
 
 void init_server() {
-   sockfd=socket(AF_INET,SOCK_DGRAM,0);
-   memset(&servaddr,0,sizeof(servaddr));
+   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+   memset(&servaddr, 0, sizeof(servaddr));
    servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-   servaddr.sin_port=htons(32000);
-   bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+   servaddr.sin_port = htons(PORT);
+   bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 }
 
 void loop_server() {
-   for (;;)
-   {
+   bool isRunning = true;
+
+   while (isRunning) {
       len = sizeof(cliaddr);
-      n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
-      sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
+      n = recvfrom(sockfd, mesg, SERVER_BUF_SIZE, 0, (struct sockaddr *)&cliaddr, &len);
+      sendto(sockfd, mesg, n, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
       printf("-------------------------------------------------------\n");
       mesg[n] = 0;
       printf("Received the following:\n");
-      printf("%s",mesg);
+      printf("%s", mesg);
       printf("-------------------------------------------------------\n");
    }
 }
